f836f244ddae028ea48376c1750a15deca4ddc41
mgokbulut <43527818+mgokbulut@users.noreply.github.com>
2019-11-20 17:06:28 +0100

    Assignment 2 is completed with 100 percent on tasks

--- Week2/Assignments_2/assignment3.java
+++ Week2/Assignments_2/assignment3.java
@@ -6,30 +6,36 @@ public class assignment3 {
 		test();
 	}
 	
+	/**
+	 * clone method for 2 dimentional array
+	 * @param a
+	 * @return
+	 */
 	static double[][] clone(double[][] a) {
 		double[][] b = new double[a.length][];
 		
 		
 		for (int i = 0; i < a.length; i++) {
 			b[i]= new double[a[i].length];
-			for (int j = 0; j < a.length - 1; j++) {
+			for (int j = 0; j < b[i].length ; j++) {
 				b[i][j] = a[i][j];
 			}
-			int k = 0;
-		}
-		
+		}		
 		
 		return b;
   }
 	
 	public static void test() {
 		double[][] a = {{0,1,2},{3,4,5,6},{7},{8, 8}};
-		double[][] b = clone(a);/*
+		double[][] b = clone(a);
+		
+		
 		for (double[] ds : b) {
 			for (int i = 0; i < ds.length; i++) {
-				System.out.println(ds[i]);
+				System.out.print(ds[i] + " ");
 			}
+			System.out.println();
 		}
-		*/
+		
 	}
 }
